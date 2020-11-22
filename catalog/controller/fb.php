272dc8b54ce70a07f779350b8c81377900013837
charoenlap <charoenlap88@gmail.com>
2020-11-22 16:03:40 +0700

    update

--- catalog/controller/fb.php
+++ catalog/controller/fb.php
@@ -21,47 +21,47 @@
 			echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
 	    }
 	    public function fbCallback($data=array()){
-	    	echo "test";
-			// $fb = new Facebook\Facebook([
-			//   'app_id' => app_id,
-			//   'app_secret' => app_secret,
-			//   'default_graph_version' => default_graph_version,
-			// ]);
+	    	echo "Connect fb.<br>";
+			$fb = new Facebook\Facebook([
+			  'app_id' => app_id,
+			  'app_secret' => app_secret,
+			  'default_graph_version' => default_graph_version,
+			]);
 
-			// $helper = $fb->getRedirectLoginHelper();
-			// try {
-			//   $accessToken = $helper->getAccessToken();
-			// } catch(Facebook\Exceptions\FacebookResponseException $e) {
-			//   // When Graph returns an error
-			//   echo 'Graph returned an error: ' . $e->getMessage();
-			//   exit;
-			// } catch(Facebook\Exceptions\FacebookSDKException $e) {
-			//   // When validation fails or other local issues
-			//   echo 'Facebook SDK returned an error: ' . $e->getMessage();
-			//   exit;
-			// }
+			$helper = $fb->getRedirectLoginHelper();
+			try {
+			  $accessToken = $helper->getAccessToken();
+			} catch(Facebook\Exceptions\FacebookResponseException $e) {
+			  // When Graph returns an error
+			  echo 'Graph returned an error: ' . $e->getMessage();
+			  exit;
+			} catch(Facebook\Exceptions\FacebookSDKException $e) {
+			  // When validation fails or other local issues
+			  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+			  exit;
+			}
 
-			// if (isset($accessToken)) {
-			//   // Logged in!
-			//   $_SESSION['facebook_access_token'] = (string) $accessToken;
+			if (isset($accessToken)) {
+			  // Logged in!
+			  $_SESSION['facebook_access_token'] = (string) $accessToken;
 
-			//   // Now you can redirect to another page and use the
-			//   // access token from $_SESSION['facebook_access_token']
+			  // Now you can redirect to another page and use the
+			  // access token from $_SESSION['facebook_access_token']
 
-			//   $response = $fb->get('/me?fields=id,name,gender,email,link', $accessToken);
+			  $response = $fb->get('/me?fields=id,name,gender,email,link', $accessToken);
 
-			//   $user = $response->getGraphUser();
-			//   echo'<pre>';
-			//   print_r($user);
-			//   echo'</pre>';
+			  $user = $response->getGraphUser();
+			  echo'<pre>';
+			  print_r($user);
+			  echo'</pre>';
 
-			  //echo 'ID: ' . $user['id'];
-			  //echo 'Name: ' . $user['name'];
-			  //echo 'Gener: ' . $user['gener'];
-			  //echo 'Email: ' . $user['email'];
-			  //echo 'Link: ' . $user['link'];
+			  echo 'ID: ' . $user['id'];
+			  echo 'Name: ' . $user['name'];
+			  echo 'Gener: ' . $user['gener'];
+			  echo 'Email: ' . $user['email'];
+			  echo 'Link: ' . $user['link'];
 
-			// }
+			}
 	    }
 	}
 ?>
