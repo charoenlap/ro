70bc3606a2725561cbb2ec511ece142951ce1b08
charoenlap <charoenlap88@gmail.com>
2020-11-23 14:20:32 +0700

    update

--- catalog/controller/fb.php
+++ catalog/controller/fb.php
@@ -38,8 +38,39 @@
 			  echo 'Facebook SDK returned an error: ' . $e->getMessage();
 			  exit;
 			}
-			// echo '<h3>Access Token</h3>';
-			// var_dump($accessToken->getValue());
+			// Logged in
+			echo '<h3>Access Token</h3>';
+			var_dump($accessToken->getValue());
+
+			// The OAuth 2.0 client handler helps us manage access tokens
+			$oAuth2Client = $fb->getOAuth2Client();
+
+			// Get the access token metadata from /debug_token
+			$tokenMetadata = $oAuth2Client->debugToken($accessToken);
+			echo '<h3>Metadata</h3>';
+			var_dump($tokenMetadata);
+
+			// Validation (these will throw FacebookSDKException's when they fail)
+			$tokenMetadata->validateAppId($config['app_id']);
+			// If you know the user ID this access token belongs to, you can validate it here
+			//$tokenMetadata->validateUserId('123');
+			$tokenMetadata->validateExpiration();
+
+			if (! $accessToken->isLongLived()) {
+			  // Exchanges a short-lived access token for a long-lived one
+			  try {
+			    $accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
+			  } catch (Facebook\Exception\SDKException $e) {
+			    echo "<p>Error getting long-lived access token: " . $e->getMessage() . "</p>\n\n";
+			    exit;
+			  }
+
+			  echo '<h3>Long-lived</h3>';
+			  var_dump($accessToken->getValue());
+			}
+
+			$_SESSION['fb_access_token'] = (string) $accessToken;
+
 			// echo 'accessToken: '.$accessToken.'<br>';
 			// if (isset($accessToken)) {
 			//   // Logged in!
